32e67914e995cff870d6f410f26bc39878551eb4
getty104 <hayabusatoshihumi@gmail.com>
2017-10-21 12:59:47 +0900

    update

--- after_bench.sh
+++ after_bench.sh
@@ -1,3 +1,3 @@
-sudo perl parse_axslog.pl /var/log/nginx/isucon5.access_log.tsv taken_sec > ~/log/nginx.log.summary
+sudo perl parse_axslog.pl /var/log/nginx/isucon7.access_log.tsv taken_sec > ~/log/nginx.log.summary
 sudo mysqldumpslow -s t /var/lib/mysql/mysqld-slow.log > ~/log/mysql.log.summary
 echo "[done] create summary."
