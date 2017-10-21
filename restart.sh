0c6035b009ae2c55d8a392f3445b8a6ebe85f575
getty104 <hayabusatoshihumi@gmail.com>
2017-10-21 17:00:18 +0900

    udpdate

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
 sudo rm /var/log/nginx/isucon7.access_log.tsv
 sudo rm /var/lib/mysql/mysqld-slow.log
 
-sudo service nginx restart
-sudo service mysql restart
-sudo service isuxi.ruby restart
+sudo systemctl restart nginx
+sudo systemctl restart mysql
+sudo systemctl restart isubata.ruby
 
 sudo sar 1 200 > ~/log/sar-cpu.log &
 sudo sar -r 1 200 > ~/log/sar-mem.log &
